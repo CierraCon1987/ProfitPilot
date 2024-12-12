8a116b2b24079a2dc13f0b53d4953319362fcf0a
CierraCon <cbaileyrice8948@conestogac.on.ca>
2024-12-12 15:28:04 -0500

    Minor Update - Remove student IDs from headers

--- add_project.php
+++ add_project.php
@@ -1,6 +1,4 @@
-<!-- Cierra Bailey-Rice (8998948)
-     Harpreet Kaur (8893116)
-     Gurkamal Singh (9001186) -->
+
 
 <?php
     session_start();
--- footer.php
+++ footer.php
@@ -1,6 +1,4 @@
-<!-- Cierra Bailey-Rice (8998948)
-     Harpreet Kaur (8893116)
-     Gurkamal Singh (9001186) -->
+
 <footer>
         <p>&copy; 2024 Freelance Project Management ProfitPilot</p>
         <p>Cierra Bailey-Rice (8998948) | Harpreet Kaur (8893116) | Gurkamal Singh (9001186)</P>
